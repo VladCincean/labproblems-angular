6cbe018d3a5c4d66b496025e10a74de398979872
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-05-01 20:00:33 +0300

    updated routing

--- labproblems-web/src/main/webapp/src/app/app-routing.module.ts
+++ labproblems-web/src/main/webapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {StudentDetailComponent} from "./students/student-detail/student-detail.c
 import {StudentsComponent} from "./students/students.component";
 import {ProblemsComponent} from "./problems/problems.component";
 import {ProblemDetailComponent} from "./problems/problem-detail/problem-detail.component";
+import {StudentNewComponent} from "./students/student-new/student-new.component";
 
 
 const routes: Routes = [
     // { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'students',     component: StudentsComponent },
     { path: 'student/detail/:id', component: StudentDetailComponent},
+    { path: 'student/new', component: StudentNewComponent},
 
     { path: 'problems',     component: ProblemsComponent },
     { path: 'problem/detail/:id', component: ProblemDetailComponent},
