a1c4ff12e3356a780ae6436d17305f24a82ee885
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-05-01 19:16:52 +0300

    student.service.ts

--- labproblems-web/src/main/webapp/src/app/students/shared/student.service.ts
+++ labproblems-web/src/main/webapp/src/app/students/shared/student.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from "@angular/http";
+import {Http, Response, Headers} from "@angular/http";
 
 import {Student} from "./student.model";
 
@@ -11,14 +11,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class StudentService {
     private studentsUrl = 'http://localhost:8080/api/students';
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) {
     }
 
     getStudents(): Observable<Student[]> {
         return this.http.get(this.studentsUrl)
-            .map(this.extractData)
-            .catch(this.handleError);
+          .map(this.extractData)
+          .catch(this.handleError);
     }
 
     private extractData(res: Response) {
@@ -27,6 +28,7 @@ export class StudentService {
     }
 
     private handleError(error: Response | any) {
+        console.log("Error (student.service.ts::handleError");
         let errMsg: string;
         if (error instanceof Response) {
             const body = error.json() || '';
@@ -39,9 +41,45 @@ export class StudentService {
         return Observable.throw(errMsg);
     }
 
+    private extractStudentData(res: Response) {
+        let body = res.json();
+        return body.student || {};
+    }
+
     getStudent(id: number): Observable<Student> {
         return this.getStudents()
             .map(students => students.find(student => student.id === id));
     }
 
+    create(serialNumber: string, name: string, studentGroup: number): Observable<Student> {
+        let student = {serialNumber, name, studentGroup};
+        return this.http
+            .post(
+                this.studentsUrl,
+                JSON.stringify({"student": student}),
+                {headers: this.headers}
+            ).map(this.extractStudentData)
+            .catch(this.handleError);
+    }
+
+    update(student): Observable<Student> {
+        const url = `${this.studentsUrl}/${student.id}`;
+        return this.http
+            .put(
+                url,
+                JSON.stringify({"student": student}),
+                {headers: this.headers}
+            ).map(this.extractStudentData)
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Observable<void> {
+        const url = `${this.studentsUrl}/${id}`;
+        return this.http
+          .delete(
+            url,
+            {headers: this.headers}
+          ).map(() => null)
+          .catch(this.handleError);
+    }
 }
