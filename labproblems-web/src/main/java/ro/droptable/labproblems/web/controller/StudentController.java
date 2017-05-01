106f805b3ec49d8a62861476d86b0484f84a7cf3
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-05-01 18:35:10 +0300

    student controller

--- labproblems-web/src/main/java/ro/droptable/labproblems/web/controller/StudentController.java
+++ labproblems-web/src/main/java/ro/droptable/labproblems/web/controller/StudentController.java
@@ -4,14 +4,20 @@ package ro.droptable.labproblems.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ro.droptable.labproblems.core.model.Student;
 import ro.droptable.labproblems.core.service.StudentService;
+import ro.droptable.labproblems.web.converter.StudentConverter;
+import ro.droptable.labproblems.web.dto.EmptyJsonResponse;
 import ro.droptable.labproblems.web.dto.StudentDto;
+import ro.droptable.labproblems.web.dto.StudentsDto;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -24,14 +30,74 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @RequestMapping(value = "/students", produces = MediaType.APPLICATION_JSON_VALUE)
-    public StudentDto getStudents() {
-        log.trace("getProblems --- method entered");
+    @Autowired
+    private StudentConverter studentConverter;
+
+    @RequestMapping(value = "/students", method = RequestMethod.GET)
+    public StudentsDto getStudents() {
+        log.trace("getStudents --- method entered");
 
         List<Student> students = studentService.findAll();
 
         log.trace("getStudents: students={}", students);
 
-        return new StudentDto(students);
+        return new StudentsDto(studentConverter.convertModelsToDtos(students));
+    }
+
+    @RequestMapping(value = "/students", method = RequestMethod.POST)
+    public Map<String, StudentDto> createStudent(
+            @RequestBody final Map<String, StudentDto> studentDtoMap
+    ) {
+        log.trace("createStudent: studentDtoMap={}", studentDtoMap);
+
+        StudentDto studentDto = studentDtoMap.get("student");
+        Student student = studentService.createStudent(
+                studentDto.getSerialNumber(),
+                studentDto.getName(),
+                studentDto.getStudentGroup()
+        );
+
+        Map<String, StudentDto> result = new HashMap<>();
+        result.put("student", studentConverter.convertModelToDto(student));
+
+        log.trace("createStudent: result={}", result);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/students/{studentId}", method = RequestMethod.PUT)
+    public Map<String, StudentDto> updateStudent(
+            @PathVariable final Long studentId,
+            @RequestBody final Map<String, StudentDto> studentDtoMap
+    ) {
+        log.trace("updateStudent: studentId={}, studentDtoMap={}", studentId, studentDtoMap);
+
+        StudentDto studentDto = studentDtoMap.get("student");
+        Student student = studentService.updateStudent(
+                studentId,
+                studentDto.getSerialNumber(),
+                studentDto.getName(),
+                studentDto.getStudentGroup()
+        );
+
+        Map<String, StudentDto> result = new HashMap<>();
+        result.put("student", studentConverter.convertModelToDto(student));
+
+        log.trace("updateStudent: result={}", result);
+
+        return result;
+    }
+
+    @RequestMapping(value = "students/{studentId}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteStudent(
+            @PathVariable final Long studentId
+    ) {
+        log.trace("deleteStudent: studentId={}", studentId);
+
+        studentService.deleteStudent(studentId);
+
+        log.trace("deleteStudent - method end");
+
+        return new ResponseEntity(new EmptyJsonResponse(), HttpStatus.OK);
     }
 }
